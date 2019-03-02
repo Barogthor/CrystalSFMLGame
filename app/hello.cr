b869f2345f7ebf3593401eeecaf4b6e71c5ef0ed
Unknown <f.thery3103@gmail.com>
2019-03-03 00:42:09 +0100

    Init window

---
+++ app/hello.cr
@@ -0,0 +1,19 @@
+require "crsfml"
+
+window = SF::RenderWindow.new(SF::VideoMode.new(800, 600), "My window")
+window.vertical_sync_enabled = true # call it once, after creating the window
+window.framerate_limit = 60 # call it once, after creating the window
+
+
+# run the program as long as the window is open
+while window.open?
+  # check all the window's events that were triggered since the last iteration of the loop
+    while event = window.poll_event
+        # "close requested" event: we close the window
+        if event.is_a? SF::Event::Closed
+            window.close
+        end
+    end
+    window.clear(SF::Color::Black)
+    window.display
+end
