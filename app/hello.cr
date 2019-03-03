625f091305d98704d873fa0ddf5f176ecc6b7015
Unknown <f.thery3103@gmail.com>
2019-03-03 01:09:23 +0100

    Creation button

--- app/hello.cr
+++ app/hello.cr
@@ -1,10 +1,13 @@
 require "crsfml"
+require "math"
+require "./widget/Button"
 
 window = SF::RenderWindow.new(SF::VideoMode.new(800, 600), "My window")
 window.vertical_sync_enabled = true # call it once, after creating the window
 window.framerate_limit = 60 # call it once, after creating the window
+button = Button.new(200,100)
 
-
+puts button.is_hover(100,150)
 # run the program as long as the window is open
 while window.open?
   # check all the window's events that were triggered since the last iteration of the loop
@@ -15,5 +18,6 @@ while window.open?
         end
     end
     window.clear(SF::Color::Black)
+    window.draw(button)
     window.display
 end
